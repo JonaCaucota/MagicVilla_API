d2f8792ccb40787b8c19b629421e9ec06474a2c5
JonaCaucota <jonacau12@gmail.com>
2023-09-19 19:33:46 -0300

    added onModelCreating

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,9 +1,17 @@
+using MagicVilla_VillaAPI.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Database
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
 Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().
     WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
