8e306fe5cf3cd3f76662bb209da726bb29b7447e
JonaCaucota <jonacau12@gmail.com>
2023-09-19 09:21:21 -0300

    added connection to database

--- MagicVilla_VillaAPI/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Dto/VillaDTO.cs
@@ -1,12 +1,20 @@
-﻿namespace MagicVilla_VillaAPI.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Dto
 {
     public class VillaDTO
     {
+        [Required]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
         public int Occupancy { get; set; }
         public int Sqft { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
 
     }
 }
