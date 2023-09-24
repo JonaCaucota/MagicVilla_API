a7b324fd1ddb9b2d4ea53f04fbd3fcf5a7b240ef
JonaCaucota <jonacau12@gmail.com>
2023-09-24 11:51:38 -0300

    added API Response

---
+++ MagicVilla_VillaAPI/Models/APIResponse.cs
@@ -0,0 +1,11 @@
+using System.Net;
+
+namespace MagicVilla_VillaAPI.Models;
+
+public class APIResponse
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; }
+    public List<string> ErrorMessage { get; set; }
+    public object Resulto { get; set; }
+}
